a102547061d9577b8b571990d6666e22a2d21919
gasello <73660655+gasello@users.noreply.github.com>
2021-01-25 23:45:47 +0300

    Homework №1

---
+++ homework1;a,b,c/homework1;c.js
@@ -0,0 +1,21 @@
+// Задание B!
+
+// Создадим шесть переменных, в каждой из которых будет разных типов данных;
+// Выводим их в alert;
+// Выводим объекты в консоль
+
+let firstName =('Zag');
+alert(firstName);
+let lastName =('Oldest');
+alert(lastName);
+let age =(132);
+alert(age);
+let zagClan =('green bread');
+alert(zagClan);
+let zagHome =('Gloktaria');
+alert(zagHome);
+let zagObject = {
+    sword: 'big sword',
+    armor: 'big armor'
+}
+console.log(zagObject);
